ccd105c42d4324700e4925d8b0c68bd2705c0317
Rasmus Pfeiffer <RasmusPfeiffer@web.de>
2022-03-31 19:26:06 +0200

    first commit

---
+++ assignment7/CustomArrayList.java
@@ -0,0 +1,59 @@
+package assignment7;
+
+//import java.util.Arrays;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	Object[] aItems = new Object[10];
+
+	@Override
+	public boolean add(T item) {
+		for (int i = 0; i < aItems.length; i++) {
+			if (aItems[i] == null) {
+				aItems[i] = item;
+				return true;
+			}
+//				else if (aItems[aItems.length - 1] != null) {
+//				aItems = Arrays.copyOf(aItems, aItems.length * 2);
+//			}
+			}
+		return false;
+	}
+
+	@Override
+	public boolean add(int index, T item) throws IndexOutOfBoundsException {
+		aItems[index] = item;
+//		if (index == aItems.length -1) {
+//			aItems = Arrays.copyOf(aItems, aItems.length + 1);
+//		}
+		for (int i = aItems.length - 1; i > index; i--) {
+			aItems[i] = aItems[i - 1];
+		}
+		return false;
+	}
+
+	@Override
+	public int getSize() {
+		int i = 0;
+		while (i < aItems.length && aItems[i] != null) {
+			i++;
+		}
+		return i;
+
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get(int index) throws IndexOutOfBoundsException {
+		return (T) aItems[index];
+	}
+
+	@Override
+	public T remove(int index) throws IndexOutOfBoundsException {
+		aItems[index] = null;
+		for (int i = index; i < aItems.length - 2; i++) {
+			aItems[i] = aItems[i + 1];
+		}
+		return null;
+	}
+
+}
---
+++ assignment7/CustomArrayListTest.java
@@ -0,0 +1,84 @@
+package assignment7;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class CustomArrayListTest {
+
+	@Test
+	void should_add_one_item() {
+		CustomList<Integer> lCustom = new CustomArrayList<>();
+		lCustom.add(1);
+		Integer iExpResult = lCustom.get(0);
+
+		assertEquals(1, iExpResult);
+	}
+	
+	@Test
+	void should_double_List_size() {
+		CustomList<Integer> lCustom = new CustomArrayList<>();
+		for (int i = 0; i < 88; i++) {
+			lCustom.add(i);
+		}
+		int iExpSize = lCustom.getSize();
+		int iExpResult = lCustom.get(87);
+		
+		assertEquals(160, iExpSize);
+		assertEquals(87, iExpResult);
+	}
+
+	@Test
+	void should_return_array_size_without_null() {
+		CustomList<Integer> lCustom = new CustomArrayList<>();
+		for (int i = 0; i < 88; i++) {
+			lCustom.add(i);
+		}
+		int iExpSize = lCustom.getSize();
+		
+		assertEquals(88, iExpSize);
+	}
+	
+	@Test
+	void should_return_array_size_without_null_if_items_equal_arrayLength() {
+		CustomList<Integer> lCustom = new CustomArrayList<>();
+		for (int i = 0; i < 40; i++) {
+			lCustom.add(i);
+		}
+		int iExpSize = lCustom.getSize();
+		
+		assertEquals(40, iExpSize);
+	}
+
+	@Test
+	void should_remove_one_item() {
+		CustomList<Integer> lCustom = new CustomArrayList<>();
+		for (int i = 0; i < 42; i++) {
+			lCustom.add(i);
+		}
+
+		lCustom.remove(4);
+
+		int iExpSize = lCustom.getSize();
+		Integer iExpResult = lCustom.get(4);
+
+		assertEquals(41, iExpSize);
+		assertEquals(5, iExpResult);
+	}
+	
+	@Test
+	void should_add_an_item_at_index() {
+		CustomList<Integer> lCustom = new CustomArrayList<>();
+		for (int i = 0; i < 40; i++) {
+			lCustom.add(i);
+		}
+		lCustom.add(5, 10);
+		
+		int iExpSize = lCustom.getSize();
+		int iExpResult = lCustom.get(5);
+		assertEquals(40+1, iExpSize);
+		assertEquals(10, iExpResult);
+	
+	}
+
+}
